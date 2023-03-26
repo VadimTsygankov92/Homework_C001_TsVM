c7f8c73efdbe9b740a2bfa1bdde7252697ac8497
Vadim Tsygankov <v-tsygankov92@mail.ru>
2023-03-26 12:52:59 +0300

    Task_6

---
+++ Task_6/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите число: ");
+int number1 = int.Parse(Console.ReadLine());
+if (number1 % 2 == 0)
+{
+  Console.WriteLine("Да!");
+}
+else
+{
+  Console.WriteLine("Нет!");
+}
