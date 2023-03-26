693d2d2387c9491c9c658ef66771d66e1acf1e36
Vadim Tsygankov <v-tsygankov92@mail.ru>
2023-03-26 12:47:47 +0300

    Task_2

---
+++ Task_2/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Введите число: ");
+int number1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int number2 = int.Parse(Console.ReadLine());
+if (number1 > number2)
+{
+  Console.WriteLine("Максимальное число: ");
+  Console.WriteLine(number1);
+  Console.WriteLine("Минимальное число: ");
+  Console.WriteLine(number2);
+} else
+{
+  Console.WriteLine("Максимальное число: ");
+  Console.WriteLine(number2);
+  Console.WriteLine("Минимальное число: ");
+  Console.WriteLine(number1);
+}
+
+
